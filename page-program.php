e3112f69008a79d9fb597f8c86aeef42a9084119
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-01-28 11:09:47 -0500

    Add all page templates

---
+++ page-program.php
@@ -0,0 +1,26 @@
+<?php /*Template Name: Program Page Template*/ ?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post();?>
+
+              <h2><?php the_title(); ?></h2>
+              <?php the_content();
+            }
+          }
+        ?>
+      </div>
+      <div class="col-md-3">
+        <?php get_sidebar(); ?>
+
+      </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>

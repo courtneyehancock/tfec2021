3b86bc1c1806354a04710850adcf50b6a1665b5f
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-01-28 10:38:11 -0500

    Edit footer size

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <div class="row">
-      <div class="nine columns">
+      <div class="col-md-9">
         <?php
           if(have_posts()){
             while(have_posts()){
@@ -14,7 +14,7 @@
           }
         ?>
       </div>
-      <div class="three columns">
+      <div class="col-md-3">
         <?php get_sidebar(); ?>
 
       </div>

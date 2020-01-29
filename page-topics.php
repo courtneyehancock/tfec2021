e77639ef088a2de4b32577d802b821f03f4566ff
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-01-29 09:45:22 -0500

    Edit footer

--- page-topics.php
+++ page-topics.php
@@ -3,22 +3,22 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="row">
-      <div class="col-md-9">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post();?>
-              <?php the_content();
-            }
+  <div class="row">
+    <div class="col-md-9">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <?php the_content();
           }
-        ?>
-      </div>
-      <div class="col-md-3">
-        <?php get_sidebar('right-sidebar'); ?>
-
-      </div>
+        }
+      ?>
     </div>
+    <div class="col-md-3">
+      <?php get_sidebar('right-sidebar'); ?>
+
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
